b4b7904c41a87e0791b6cef9a2ec74a1cfbd848e
James Porter <james@jamesporter.me>
2017-01-18 22:32:40 -0500

    use better reprs, hackily fix a bunch of bugs

--- lisp.py
+++ lisp.py
@@ -9,25 +9,35 @@ T = TypeVar('T')
 class LispData(object):
     pass
 
-
 class Symbol(str, LispData):
-    pass
+    def __repr__(self):
+        return f"Symbol({super().__repr__()})"
 
 
 class String(str, LispData):
-    pass
+    def __repr__(self):
+        return f"String({super().__repr__()})"
+
+
+class Bool(int, LispData):  # cant inherit from bool
+    def __repr__(self):
+        to_show = "true" if self else "false"
+        return f"Bool({to_show})"
 
 
 class List(list, LispData, Generic[T]):
-    pass
+    def __repr__(self):
+        return f"List({super().__repr__()})"
 
 
 class Int(int, LispData):
-    pass
+    def __repr__(self):
+        return f"Int({super().__repr__()})"
 
 
 class Float(float, LispData):
-    pass
+    def __repr__(self):
+        return f"Float({super().__repr__()})"
 
 
 def tokenize(chars: str) -> L[str]:
@@ -35,15 +45,27 @@ def tokenize(chars: str) -> L[str]:
     return chars.replace('(', ' ( ').replace(')', ' ) ').split()
 
 
+def parse(program):
+    "Read a Scheme expression from a string."
+    tokens = tokenize(program)
+    parsed = read_from_tokens(tokens)
+    if tokens:
+        raise SyntaxError(f"unexpected input: {tokens}")
+    return parsed
+
+
 def read_from_tokens(tokens: L[str]) -> LispData:
     "Read an expression from a sequence of tokens."
-    if len(tokens) == 0:
+    print(tokens)
+    if not tokens:
         raise SyntaxError('unexpected EOF while reading')
     token = tokens.pop(0)
     if '(' == token:
-        acc: List[LispData] = List()  # flake8: noqa
-        while tokens[0] != ')':
+        acc: List[LispData] = List()
+        while tokens and tokens[0] != ')':
             acc.append(read_from_tokens(tokens))
+        if not tokens:
+            raise SyntaxError("unexpected EOF while reading")
         tokens.pop(0)  # pop off ')'
         return acc
     elif ')' == token:
@@ -60,7 +82,16 @@ def atom(token: str) -> LispData:
             return Float(float(token))
         except ValueError:
             # TODO: chars?
-            if token.startswith('"'):
-                return String(token)
+            if token == "true":
+                return Bool(True)
+            elif token == "false":
+                return Bool(False)
+            elif token.startswith('"'):
+                if not token.endswith('"'):
+                    raise SyntaxError(f"unclosed string: {token}")
+                s = token[1:-1]
+                if '"' in s:
+                    raise SyntaxError(f"invalid string: {token}")
+                return String(s)
             else:
                 return Symbol(token)
