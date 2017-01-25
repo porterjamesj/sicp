90a0343addbfbe437670a0d2b9771bc4e4655b32
James Porter <james@jamesporter.me>
2017-01-25 00:22:15 -0500

    finish representing expressions, procedures
    need to do environments

--- lisp.py
+++ lisp.py
@@ -4,6 +4,13 @@ from typing import TypeVar, Generic
 T = TypeVar('T')
 
 
+class EvalError(Exception):
+
+    def __init__(self, msg, data):
+        self.msg = msg
+        self.data = data
+
+
 # Lisp datatypes
 
 class LispData(object):
@@ -147,14 +154,16 @@ def cdddar(x): return cdr(cdr(cdr(car(x))))
 def cddddr(x): return cdr(cdr(cdr(cdr(x))))
 
 
-
-
 # evaluator
+
 
 python_eval = eval  # why not
 
 
-# TODO types?
+
+# eval and apply
+
+
 def eval(exp, env):
     if is_self_evaluating(exp):
         return exp
@@ -178,7 +187,7 @@ def eval(exp, env):
     elif is_application(exp):
         apply(eval(operator(exp), env), list_of_values(operands(exp), env))
     else:
-        raise RuntimeError("unknown expression type -- EVAL", exp)
+        raise EvalError("unknown expression type -- EVAL", exp)
 
 
 def apply(procedure, arguments):
@@ -191,9 +200,10 @@ def apply(procedure, arguments):
                                arguments, procedure_environment(procedure))
         )
     else:
-        raise RuntimeError("Unknown procedure type -- APPLY", procedure)
+        raise EvalError("Unknown procedure type -- APPLY", procedure)
 
 
+# representing expressions
 
 # TODO this code evaluates operands from left to right. Exercise 4.1
 # is to write a version that evaluates from right to left
@@ -224,16 +234,24 @@ def eval_sequence(exps, env):
 
 def eval_assignment(exp, env):
     # TODO is [] syntax going to work for this?
-    env[assignment_variable(exp)] = eval(assignment_value(exp), env)
+    set_variable_value(
+        assignment_variable(exp),
+        eval(assignment_value(exp), env),
+        env
+    )
     return Symbol("ok")  # XXX
 
 
 def eval_definition(exp, env):
-    env[definition_variable(exp)] = eval(definition_value(exp), env)
-    return Symbol("ok")
+    define_variable(
+        definition_variable(exp),
+        eval(definition_value(exp), env),
+        env
+    )
+    return Symbol("ok")  # XXX
 
 
-def self_evaluating(exp):
+def is_self_evaluating(exp):
     # NOTE our lisp has bools and apparently the one in the book
     # doesn't? this is probably ok
     return (isinstance(exp, Float) or isinstance(exp, Int)
@@ -246,6 +264,10 @@ def is_variable(exp):
 
 def is_quoted(exp):
     return tagged_list(exp, Symbol("quote"))
+
+
+def text_of_quotation(exp):
+    return cadr(exp)
 
 
 def tagged_list(exp, tag):
@@ -325,3 +347,136 @@ def if_alternative(exp):
 
 def make_if(predicate, consequent, alternative):
     List([Symbol("if"), predicate, consequent, alternative])
+
+
+def is_begin(exp):
+    return tagged_list(exp, Symbol("Begin"))
+
+
+def begin_actions(exp):
+    return cdr(exp)
+
+
+def is_last_exp(seq):
+    return bool(cdr(seq))
+
+
+def first_exp(seq):
+    return car(seq)
+
+
+def rest_exps(seq):
+    return cdr(seq)
+
+
+def sequence_to_exp(seq):
+    if not seq:
+        return seq
+    elif is_last_exp(seq):
+        return first_exp(seq)
+    else:
+        return make_begin(seq)
+
+
+def make_begin(seq):
+    return cons(Symbol("begin"), seq)
+
+
+def is_application(seq):
+    # TODO in the book this is `pair?`, so this is a bit bogus becuase
+    # I don't have pairs but i think it will fine?
+    return isinstance(seq, List)
+
+
+def operator(exp):
+    return car(exp)
+
+
+def operands(exp):
+    cdr(exp)
+
+
+def no_operands(ops):
+    return bool(ops)
+
+
+def first_operand(ops):
+    return car(ops)
+
+
+def rest_operands(ops):
+    return cdr(ops)
+
+
+def is_cond(exp):
+    return tagged_list(exp, Symbol("cond"))
+
+
+def cond_clauses(exp):
+    return cdr(exp)
+
+
+def is_cond_else_clause(clause):
+    return cond_predicate(clause) == Symbol("else")
+
+
+def cond_predicate(clause):
+    return car(clause)
+
+
+def cond_actions(clause):
+    return cdr(clause)
+
+
+def cond_to_if(exp):
+    return expand_clauses(cond_clauses(exp))
+
+
+def expand_clauses(clauses):
+    if not clauses:
+        # TODO the book does not make it clear whether this should be
+        # implemented-false or implementor-false, but I think the
+        # former
+        return LispFalse
+    else:
+        first = car(clauses)
+        rest = cdr(clauses)
+        if is_cond_else_clause(first):
+            if not rest:
+                return sequence_to_exp(cond_actions(first))
+            else:
+                raise EvalError("ELSE clause isn't last -- COND->IF", clauses)
+        else:
+            return make_if(
+                cond_predicate(first),
+                sequence_to_exp(cond_actions(first)),
+                expand_clauses(rest)
+            )
+
+
+# evaluator data structures
+
+# procedures
+
+def apply_primitive_procedure(proc, args):
+    pass  # TODO
+
+
+def is_primitive_procedure(proc):
+    pass  # TODO
+
+
+def make_procedure(parameters, body, env):
+    return List([Symbol("procedure")], parameters, body, env)
+
+
+def procedure_parameters(p):
+    return cadr(p)
+
+
+def procedure_body(p):
+    return caddr(p)
+
+
+def procedure_environment(p):
+    return cadddr(p)
